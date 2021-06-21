ab640cadf852d70a67ba711807e19c9f68fa5987
Hiep Tran <tranquyhiep87@gmail.com>
2021-06-21 11:29:15 +0700

    Update ci

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -1,19 +1,23 @@
 #!/bin/bash
+# export SSH_REMOTE_SERVER_USER=mage
+# export SSH_REMOTE_SERVER_IP=112.109.73.217
+# export SSH_REMOTE_SERVER_HOST=112.109.73.217
+# export SSH_REMOTE_SERVER_ROOT=/var/www/staging/magento-demo
+# export CIRCLE_PROJECT_REPONAME=woocommerce-latitudepay-genoapay
 
 INVALID_PARAMETERS="\033[1;31mError:\033[0m Please make sure you've indicated correct parameters."
 
 # Path to plugin directory on the remote server
-PLUGIN_DIR="/path/on/remote/server/to/wp-content/plugins/${CIRCLE_PROJECT_REPONAME}"
+PLUGIN_DIR="${SSH_REMOTE_SERVER_ROOT}/app/code/Latitude/Payment"
 
 # Set Remote server SSH credentials (If this is a public Repo, you will want to set these as CircleCI environment variables)
-SSH_CREDS="user@host"
+SSH_CREDS="${SSH_REMOTE_SERVER_USER}@${SSH_REMOTE_SERVER_HOST}"
 
 # Add the server IP to the known_hosts file
 # Required for CircleCI to allow SSH connections to remote server
 # example: ssh-keyscan 123.456.789.123 >> ~/.ssh/known_hosts
-ssh-keyscan REMOTE.SERVER.IP >> ~/.ssh/known_hosts
-
-cd ${HOME}/project/build/${CIRCLE_PROJECT_REPONAME}
+ssh-keyscan ${SSH_REMOTE_SERVER_IP} >> ~/.ssh/known_hosts
+cd ~/latitude
 
 if [ $# -eq 0 ]
 	then
@@ -21,11 +25,13 @@ if [ $# -eq 0 ]
 elif [ $1 == "--dry-run" ]
 	then
 		echo "Running dry-run deployment."
-		rsync --stats --dry-run --delete --progress -avz -e "ssh -p22" ./ ${SSH_CREDS}:${PLUGIN_DIR}
+		ssh -p22 ${SSH_CREDS} mkdir -p ${PLUGIN_DIR}
+		rsync --exclude={'.git/','bin/','.docker/','vendor/','log/','tests/*','.vscode/','.circleci/','.idea/'} --stats --dry-run --delete --progress -avz -e "ssh -p22" ./ ${SSH_CREDS}:${PLUGIN_DIR}
 elif [ $1 == "live" ]
 	then
 		echo "Running actual deploy"
-		rsync --stats --delete -avz -e "ssh -p22" ./ ${SSH_CREDS}:${PLUGIN_DIR}
+		ssh -p22 ${SSH_CREDS} mkdir -p ${PLUGIN_DIR}
+		rsync --exclude={'.git/','bin/','.docker/','vendor/','log/','tests/','.vscode/','.circleci/','.idea/'} --stats --delete -avz -e "ssh -p22" ./ ${SSH_CREDS}:${PLUGIN_DIR}
 else
 	echo -e ${INVALID_PARAMETERS};
 fi

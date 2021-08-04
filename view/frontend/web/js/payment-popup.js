2b0b2763a2aaac822171c6735d0a0db6f2eb99db
Hiep Tran <tranquyhiep87@gmail.com>
2021-08-04 17:54:51 +0700

    Update

--- view/frontend/web/js/payment-popup.js
+++ view/frontend/web/js/payment-popup.js
@@ -1,34 +1,14 @@
-define(['jquery','Magento_Ui/js/modal/modal'],function($,modal) {
+define(['jquery'],function($) {
     'use strict';
     return function(config, element) {
         function initPaymentOptionPopup () {
-            var e = document.querySelectorAll("img[src*='https://images.latitudepayapps.com/v2/snippet.svg'], img[src*='https://images.latitudepayapps.com/v2/api/banner'], img[src*='https://images.latitudepayapps.com/v2/LatitudePayPlusSnippet.svg']");
-            [].forEach.call(
-                e, function (e) {
-                    e.style.cursor = "pointer",
-                        e.addEventListener("click", handleClick)
-                })
-            function handleClick(e) {
-                if (0 == document.getElementsByClassName("lpay-modal-wrapper").length) {
-                    var t = new XMLHttpRequest;
-                    t.onreadystatechange = function () {
-                        4 == t.readyState && 200 == t.status && null != t.responseText && (document.body.insertAdjacentHTML("beforeend", t.responseText))
-                        var lpaySvgModal = document.querySelector('.lpay-modal svg');
-                        if(lpaySvgModal){
-                            var ua = window.navigator.userAgent;
-                            var msie = ua.indexOf('MSIE ');
-                            lpaySvgModal.style.width = '100%';
-                            if (msie <= 0){
-                                lpaySvgModal.style.height = '100%';
-                            }
-                        }
-                    },
-                        t.open("GET", e.srcElement.currentSrc.replace('snippet.svg','modal.html'), !0),
-                        t.send(null)
-                } else document.querySelector(".lpay-modal-wrapper").style.display = "block"
-            }
+            $("img[src*='https://images.latitudepayapps.com/v2/snippet.svg'], img[src*='https://images.latitudepayapps.com/v2/api/banner'], img[src*='https://images.latitudepayapps.com/v2/LatitudePayPlusSnippet.svg']").click(function(){
+                var url = $(this).attr('src').replace('snippet.svg','modal.html');
+                $.get(url,function(html){
+                    $( "body" ).append(html);
+                });
+            })
         }
-
         $(document).ready(function () {
             initPaymentOptionPopup();
         });
--- view/frontend/web/js/view/payment/method-renderer/genoapay.js
+++ view/frontend/web/js/view/payment/method-renderer/genoapay.js
@@ -24,35 +24,16 @@ define(
                 this._super();
                 var _self = this;
                 _self.PaymentFaileMsg();
-                setTimeout(this.initPopup,2000);
+                setTimeout(this.initPopup,4000);
                 return this;
             },
             initPopup: function() {
-                var e = document.querySelectorAll("img[src*='https://images.latitudepayapps.com/v2/snippet.svg'], img[src*='https://images.latitudepayapps.com/v2/api/banner'], img[src*='https://images.latitudepayapps.com/v2/LatitudePayPlusSnippet.svg']");
-                [].forEach.call(
-                    e, function (e) {
-                        e.style.cursor = "pointer",
-                            e.addEventListener("click", handleClick)
-                    })
-                function handleClick(e) {
-                    if (0 == document.getElementsByClassName("lpay-modal-wrapper").length) {
-                        var t = new XMLHttpRequest;
-                        t.onreadystatechange = function () {
-                            4 == t.readyState && 200 == t.status && null != t.responseText && (document.body.insertAdjacentHTML("beforeend", t.responseText));
-                            var lpaySvgModal = document.querySelector('.lpay-modal svg');
-                            if(lpaySvgModal){
-                                var ua = window.navigator.userAgent;
-                                var msie = ua.indexOf('MSIE ');
-                                lpaySvgModal.style.width = '100%';
-                                if (msie <= 0){
-                                    lpaySvgModal.style.height = '100%';
-                                }
-                            }
-                        },
-                            t.open("GET", e.srcElement.currentSrc.replace('snippet.svg','modal.html'), !0),
-                            t.send(null)
-                    } else document.querySelector(".lpay-modal-wrapper").style.display = "block"
-                }
+                $("img[src*='https://images.latitudepayapps.com/v2/snippet.svg'], img[src*='https://images.latitudepayapps.com/v2/api/banner'], img[src*='https://images.latitudepayapps.com/v2/LatitudePayPlusSnippet.svg']").click(function(){
+                    var url = $(this).attr('src').replace('snippet.svg','modal.html');
+                    $.get(url,function(html){
+                        $( "body" ).append(html);
+                    });
+                })
             },
             /** Returns send check to info */
             getMailingAddress: function() {

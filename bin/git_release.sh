9d49a3ecea5296b556cfd79cb4f04528d0189a2f
hieptran87 <63086258+hieptran87@users.noreply.github.com>
2021-10-29 10:53:39 +0700

    Update release script

--- bin/git_release.sh
+++ bin/git_release.sh
@@ -1,7 +1,13 @@
 #!/usr/bin/env bash
-cd ..
 set -e
-SLUG=${PWD##*/}
+REPO_URL=$(git config --get remote.origin.url)
+
+RE="^(https|git)(:\/\/|@)([^\/:]+)[\/:]([^\/:]+)\/(.+).git$"
+
+if [[ $REPO_URL =~ $RE ]]; then
+    GITHUB_USER=${BASH_REMATCH[4]}
+    GITHUB_REPO=${BASH_REMATCH[5]}
+fi
 EXISTING_TAGS=(`echo $(git tag -l)`);
 COMPOSER_TAG=$(grep -o '^ *"version": *"[0-9\.]*"' composer.json|awk '{print $2}'|sed -e 's/"\(.*\)"/\1/g')
 for constant in $EXISTING_TAGS
@@ -10,12 +16,11 @@ do
     echo "The tag exists already: ${COMPOSER_TAG}";
     exit;
   fi
-
 done
 echo "Create new tag: ${COMPOSER_TAG}"
 CHANGELOG_JSON="Create new tag: ${COMPOSER_TAG}"
 API_JSON=$(printf '{"tag_name": "%s","target_commitish": "ci-cd","name": "%s","body": "%s","draft": false,"prerelease": false}' $COMPOSER_TAG $COMPOSER_TAG "$CHANGELOG_JSON")
 
-curl --data "$API_JSON" https://api.github.com/repos/${GITHUB_USER}/${SLUG}/releases?access_token=${GITHUB_TOKEN}
+curl -H "Authorization: token ${GITHUB_TOKEN}" --data "$API_JSON" https://api.github.com/repos/${GITHUB_USER}/${GITHUB_REPO}/releases
 
 echo "*** FIN ***"
